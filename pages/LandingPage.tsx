324e3dbfba93d220231dcbf49afb56a4ddef4cf1
Emilio Miralles <emilio_miralles@sfu.ca>
2023-04-09 02:13:31 +0200

    Refactor to have separate LandingPage. Use papaparse instead of d3.

---
+++ pages/LandingPage.tsx
@@ -0,0 +1,87 @@
+import { useEffect, useState } from 'react';
+import { createStyles, rem, Container, Title, Grid } from '@mantine/core';
+const Papa = require('papaparse');
+
+import JobCard from './JobCard';
+
+const useStyles = createStyles((theme) => ({
+  wrapper: {
+    position: 'relative',
+    paddingTop: rem(120),
+    paddingBottom: rem(80),
+
+    [theme.fn.smallerThan('sm')]: {
+      paddingTop: rem(80),
+      paddingBottom: rem(60),
+    },
+  },
+
+  inner: {
+    position: 'relative',
+    paddingTop: rem(120),
+    paddingBottom: rem(80),
+
+    [theme.fn.smallerThan('sm')]: {
+      paddingTop: rem(80),
+      paddingBottom: rem(60),
+    },
+  },
+
+  title: {
+    textAlign: 'center',
+    fontWeight: 800,
+    fontSize: rem(40),
+    letterSpacing: -1,
+
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    
+    marginBottom: theme.spacing.xs,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+
+    [theme.fn.smallerThan('xs')]: {
+      fontSize: rem(28),
+      textAlign: 'left',
+    },
+  },
+}));
+
+export default function LandingPage({ url }) {
+  const { classes } = useStyles()
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const csvText = await response.text();
+        const theCsv = Papa.parse(csvText, {
+          header: true
+        });
+        setData([...theCsv.data]);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <main>
+      <Container className={classes.wrapper} size={1400}>
+        <div className={classes.inner}>
+          <Title className={classes.title}>
+            Resource Generation Job Board
+          </Title>
+        </div>
+      </Container>
+      <Container>
+        <Grid>
+          {data.map((d, k) => (
+            <Grid.Col span={4} key={k}>
+              <JobCard data={d}></JobCard>
+            </Grid.Col>))}
+        </Grid>
+      </Container>
+    </main>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,20 @@
+import Head from 'next/head';
 import { Button, Group, Container, Title, Grid } from "@mantine/core";
-import { csv } from 'd3-fetch';
-import { DSVRowArray } from 'd3-dsv';
 import { useEffect, useState } from 'react';
-import JobCard from './JobCard';
+
+import LandingPage from './LandingPage'
 
 export default function IndexPage() {
   const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQTIAE4rAi3VwxQ3zSHlBgZNg43gfMVJ-uYiXFDvdDHNQMYPTNyir155Vbv2o2KacdYb8BZSSIJI88A/pub?gid=0&single=true&output=csv";
-  const [data, setData] = useState(DSVRowArray<string>);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const theCsv = await csv(url);
-        console.log("theCsv", theCsv);
-        setData([...theCsv]);
-      } catch (error) {
-        console.log("error", error);
-      }
-    };
-    fetchData();
-  }, []);
-
   return (
-    <Group mt={50} position="center">
-      <Container className={classes.wrapper} size={1400}>
-        <div className={classes.inner}>
-          <Title className={classes.title}>
-            Resource Generation Job Board
-          </Title>
-        </div>
-      </Container>
-      <Container>
-        <Grid>
-          {data.map(d => (
-            <Grid.Col span={4} key={d.title}>
-              <JobCard data={d}></JobCard>
-            </Grid.Col>))}
-        </Grid>
-      </Container>
-    </Group>
+    <>
+      <Head>
+        <title>ListingBoard</title>
+        <meta name="description" content="A board of listings" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <LandingPage url={url} />
+    </>
   );
 }

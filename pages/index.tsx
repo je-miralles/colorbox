8b2b47710034cd4e79f33915d36059e747d4f010
Emilio Miralles <emilio_miralles@sfu.ca>
2023-04-09 04:14:39 +0200

    Move url to defaultProps.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,9 @@ import { Group } from "@mantine/core";
 import LandingPage from './LandingPage';
 
 export default function IndexPage() {
-  const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQTIAE4rAi3VwxQ3zSHlBgZNg43gfMVJ-uYiXFDvdDHNQMYPTNyir155Vbv2o2KacdYb8BZSSIJI88A/pub?gid=0&single=true&output=csv";
   return (
     <Group mt={50} position="center">
-      <LandingPage url={url} />
+      <LandingPage />
     </Group>
   );
 }

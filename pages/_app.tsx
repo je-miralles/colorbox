701f1aa585f26973e2136316bd8f059042c0087a
Emilio Miralles <emilio_miralles@sfu.ca>
2023-04-09 21:05:46 +0200

    tryfix favicon.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App(props: AppProps) {
     <>
       <Head>
         <title>RG Job Board</title>
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="shortcut icon" href="favicon.svg" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"

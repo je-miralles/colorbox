4901d3e03480e5b1e82a26ad4311936761e2a5d4
Emilio Miralles <emilio_miralles@sfu.ca>
2023-04-20 03:22:03 +0200

    Change to green.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,16 +24,16 @@ export default function App(props: AppProps) {
           colorScheme: "dark",
           colors: {
             dark: [
-              '#fff9db', // text color
-              '#fff3bf',
-              '#ffec99',
-              '#ffe066',
-              '#ffd43b',
-              '#fcc419',
-              '#fab005',
-              '#f59f00', // body background color
-              '#f08c00',
-              '#e67700',
+              '#ebfbee', // text color
+              '#d3f9d8',
+              '#b2f2bb',
+              '#8ce99a',
+              '#69db7c',
+              '#51cf66',
+              '#40c057',
+              '#37b24d', // body background color
+              '#2f9e44',
+              '#2b8a3e',
             ],
           }
         }}

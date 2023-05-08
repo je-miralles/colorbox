5085769c2ba244e1e575202ad3a7fbb7c75a17b3
Emilio Miralles <emilio_miralles@sfu.ca>
2023-05-08 20:04:48 +0200

    Expose gray colors for tweaking.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,6 +35,18 @@ export default function App(props: AppProps) {
               '#0c0d21',
               '#01010a',
             ],
+            gray: [
+              '#f8f9fa',
+              '#f1f3f5',
+              '#e9ecef',
+              '#dee2e6',
+              '#ced4da',
+              '#adb5bd',
+              '#868e96',
+              '#495057',
+              '#343a40',
+              '#212529',
+            ],
           }
         }}
       >

b4b3147e037bcb6cfae5a4667f9341e42f813e74
Emilio Miralles <emilio_miralles@sfu.ca>
2023-05-13 13:55:10 +0200

    Responsiveness.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,7 +23,7 @@ export default function App(props: AppProps) {
           colorScheme: "light",
           breakpoints: {
             xs: '16em',
-            sm: '28em',
+            sm: '29em',
             md: '40em',
             lg: '60em',
             xl: '88em',

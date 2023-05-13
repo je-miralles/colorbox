82d788cdf46d27307a90ee9e57cb9a11bb596005
Emilio Miralles <emilio_miralles@sfu.ca>
2023-05-13 03:31:30 +0200

    Small up xs breakpoint..

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,7 +23,7 @@ export default function App(props: AppProps) {
           colorScheme: "light",
           breakpoints: {
             xs: '16em',
-            sm: '27em',
+            sm: '28em',
             md: '40em',
             lg: '60em',
             xl: '88em',
